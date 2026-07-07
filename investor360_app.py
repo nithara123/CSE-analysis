a92fd230a70dda9859e993a66cfe26f8489c2291
nithara123 <kithsandinithara24@gmail.com>
2026-07-07 10:52:17 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- investor360_app.py
+++ investor360_app.py
@@ -4,7 +4,6 @@ import random
 import base64
 import pandas as pd
 import plotly.graph_objects as go
-from cse_price_chart import render_price_movement_section
 
 st.set_page_config(page_title="Investor 360 | CSE Analytics", layout="wide", initial_sidebar_state="expanded")
 
